2294712bac16b30f8e4fcf7c4f6ff71308d365f1
Ye Lwin Htay <yelwin.r.k@gmail.com>
2022-10-15 07:46:32 -0700

    Add files via upload

---
+++ practice-for-week-02-pojo_basics/problems/09-apple-counter.js
@@ -0,0 +1,64 @@
+/***********************************************************************
+Write a function `appleCounter(appleObj)` that takes in an object containing a
+number of keys that have the word 'apple' contained within them. The `appleCounter`
+function will be in charge of returning the number of keys that contain the word
+"apple".
+
+**Hint**: you may want to take all the keys and lower case them for easier
+searching.
+
+
+Example:
+let obj = { banana: "yay!" };
+appleCounter(obj); // => 0
+
+let obj1 = { crabapple: "yum!" };
+appleCounter(obj1); // => 1
+
+let obj2 = { crabapple: "yum!", honeyapple: "super yum", banana: "yay" };
+appleCounter(obj2); // => 2
+
+
+let obj3 = {
+  crabApple: "yum!",
+  honeyApple: "super yum",
+  banana: "yay",
+  bigapple: "NYC"
+};
+appleCounter(obj3); // => 3
+***********************************************************************/
+
+function appleCounter(appleObj) {
+	let count = 0;
+
+	let value = Object.keys(appleObj);
+	// console.log(value);
+	value.forEach( word => {
+		console.log(word);
+		if (word.toLowerCase().includes('apple')) {
+			count += 1
+		}
+	})
+	return count;
+}
+
+let obj = { banana: "yay!" };
+appleCounter(obj); // => 0
+
+let obj1 = { crabapple: "yum!" };
+appleCounter(obj1); // => 1
+
+let obj2 = { crabapple: "yum!", honeyapple: "super yum", banana: "yay" };
+appleCounter(obj2); // => 2
+
+
+let obj3 = {
+  crabApple: "yum!",
+  honeyApple: "super yum",
+  banana: "yay",
+  bigapple: "NYC"
+};
+appleCounter(obj3); // => 3
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+module.exports = appleCounter;
